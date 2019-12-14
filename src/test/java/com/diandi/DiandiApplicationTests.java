9b27c371d32d986c10878f4181162065bca10c59
you19345 <1934508337@qq.com>
2019-12-14 19:17:12 +0800

    逆向工程生成基本实体

---
+++ src/test/java/com/diandi/DiandiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diandi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiandiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
