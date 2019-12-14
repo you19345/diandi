1520e5890af11854900c78fd0b1c80977b79f841
you19345 <1934508337@qq.com>
2019-12-14 17:59:45 +0800

    逆向工程生成基本实体

---
+++ src/main/java/com/diandi/user/dao/UserMapper.java
@@ -0,0 +1,21 @@
+package com.diandi.user.dao;
+
+import com.diandi.user.entity.User;
+
+import java.util.List;
+
+public interface UserMapper {
+    int deleteByPrimaryKey(Double id);
+
+    int insert(User record);
+
+    int insertSelective(User record);
+
+    List<User> selectByExample(UserExample example);
+
+    User selectByPrimaryKey(Double id);
+
+    int updateByPrimaryKeySelective(User record);
+
+    int updateByPrimaryKey(User record);
+}
